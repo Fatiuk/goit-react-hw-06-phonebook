cfcf6d17809821c57bf80c0da26d08672172bf33
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-10-06 02:30:02 +0300

    Update 0.1.10
    (Default Redux)

---
+++ src/redux/store.jsx
@@ -0,0 +1,17 @@
+import { createStore } from 'redux';
+
+const initialState = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filter: '',
+};
+
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+
+export const store = createStore(rootReducer);
